2b2cae6206008c9b7c6f552d56b2417f01340334
Kamil Malik <km8003296@gmail.com>
2022-10-15 14:57:50 +0700

    Basic Register Logic

--- app/src/main/java/com/wirajasa/wirajasabisnis/ui/register_buyyer/RegisterBuyyerActivity.kt
+++ app/src/main/java/com/wirajasa/wirajasabisnis/ui/register_buyyer/RegisterBuyyerActivity.kt
@@ -1,4 +1,4 @@
-package com.wirajasa.wirajasabisnis.ui.login
+package com.wirajasa.wirajasabisnis.ui.register_buyyer
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,39 +6,31 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.wirajasa.wirajasabisnis.databinding.ActivityLoginBinding
+import androidx.lifecycle.lifecycleScope
+import com.wirajasa.wirajasabisnis.databinding.ActivityRegisterBuyyerBinding
 import com.wirajasa.wirajasabisnis.ui.main_activity.MainActivity
-import com.wirajasa.wirajasabisnis.ui.register_buyyer.RegisterBuyyerActivity
-import com.wirajasa.wirajasabisnis.ui.reset_password.ResetPasswordActivity
 import com.wirajasa.wirajasabisnis.utility.NetworkResponse
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 
 @AndroidEntryPoint
-class LoginActivity : AppCompatActivity(), View.OnClickListener {
+class RegisterBuyyerActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var _binding: ActivityLoginBinding? = null
+    private var _binding: ActivityRegisterBuyyerBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel by viewModels<LoginViewModel>()
+    private val viewModel by viewModels<RegisterBuyyerViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        _binding = ActivityRegisterBuyyerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (viewModel.getCurrentUser() != null) {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-
         binding.apply {
-            btnResetPassword.setOnClickListener(this@LoginActivity)
-            btnLogin.setOnClickListener(this@LoginActivity)
-            btnRegister.setOnClickListener(this@LoginActivity)
+            btnRegister.setOnClickListener(this@RegisterBuyyerActivity)
         }
 
-        viewModel.loginStatus.observe(this) {
+        viewModel.registerStatus.observe(this) {
             when (it) {
                 is NetworkResponse.FirebaseException -> Toast.makeText(
                     this,
@@ -53,10 +45,17 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                     Toast.LENGTH_SHORT
                 ).show()
                 is NetworkResponse.Success -> {
-                    Toast.makeText(this, "Sukses Login", Toast.LENGTH_SHORT).show()
-                    startActivity(Intent(this, MainActivity::class.java))
+                    Toast.makeText(this, "Sukses Register", Toast.LENGTH_SHORT).show()
+                    if (viewModel.getCurrentUser() != null)
+                        startActivity(Intent(this, MainActivity::class.java))
                 }
                 else -> return@observe
+            }
+        }
+
+        lifecycleScope.launchWhenCreated {
+            viewModel.emailError.collect(){
+                binding.edtEmail.error = it
             }
         }
     }
@@ -64,16 +63,17 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         val email = binding.edtEmail.text.toString()
         val password = binding.edtPassword.text.toString()
-
+        val confirmedPassword = binding.edtConfirmPassword.text.toString()
         when (v?.id) {
-            binding.btnLogin.id -> viewModel.signInWithEmailAndPassword(email, password, Dispatchers.IO)
-            binding.btnResetPassword.id -> startActivity(
-                Intent(
-                    this,
-                    ResetPasswordActivity::class.java
-                ).also { finish() }
-            )
-            binding.btnRegister.id -> startActivity(Intent(this, RegisterBuyyerActivity::class.java))
+            binding.btnRegister.id -> {
+                if (email.isNotEmpty() && (password == confirmedPassword)) viewModel
+                    .signUpUserWithEmailAndPassword(
+                        email,
+                        password,
+                        Dispatchers.IO
+                    )
+                else return
+            }
         }
     }
 
