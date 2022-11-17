55d44e6eeebb353f84076cf3c2557c6d5e1eda3a
Annisa Mahfuzhah <2010817220018@mhs.ulm.ac.id>
2022-11-17 21:53:52 +0800

    first commit
    first commit

---
+++ app/src/main/java/com/wirajasa/wirajasabisnis/ui/user/UserValidation.kt
@@ -0,0 +1,11 @@
+package com.wirajasa.wirajasabisnis.ui.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserValidation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_validation)
+    }
+}
