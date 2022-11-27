46496961968443be329641fe2cdd28e3c3c54534
Ishak4875 <ishakjaelani07@gmail.com>
2022-11-27 20:25:13 +0800

    melakukan perubahan pada layout detail activity

---
+++ app/src/main/java/com/wirajasa/wirajasabisnis/presentation/service/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.wirajasa.wirajasabisnis.presentation.service
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wirajasa.wirajasabisnis.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
